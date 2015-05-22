4601e1eef4c6414839d18f16721dc8706f710327
Herman Lundkvist <herlu184@student.liu.se>
2015-05-22 22:15:10 +0200

    lsadjfls

--- TSKS10/labb.m
+++ TSKS10/labb.m
@@ -10,14 +10,14 @@ t = 0:1/Fs:(N-1)*1/Fs;
 % this section
 % 
 y_c = xcorr(y, y);
-plot(y_c);
+x_c = linspace(-7.8e+06, 7.8e+06, 15599999);
+plot(x_c ,y_c);
 
-axis([7*10^6 8.6*10^6 -1500 2000])
+axis([-10^6 10^6 -1500 2000])
 set(gca,'FontSize',20)
 xlabel('\bf{n:te sampel}', 'Interpreter', 'Latex', 'FontSize', 24);
 ylabel('$\mathbf{r[n]}$', 'Interpreter', 'Latex', 'FontSize', 24);
 print('fig1','-dpdf');
-
 
 lpeak = 7.644*10^6;
 mpeak = 7.800*10^6;
@@ -59,14 +59,25 @@ X_target3 = ze;
 X_target1(0.2/2*N/2:0.6/2*N/2) = X(0.2/2*N/2:0.6/2*N/2);
 X_target2(0.7/2*N/2:1.1/2*N/2) = X(0.7/2*N/2:1.1/2*N/2);
 X_target3(1.3/2*N/2:1.7/2*N/2) = X(1.3/2*N/2:1.7/2*N/2);
+x_target1 = ifft(X_target1, 'symmetric');
+x_target2 = ifft(X_target2, 'symmetric');
 X_target = X_target3;
 x_target = ifft(X_target, 'symmetric');
 
+subplot(3,1,1)
+plot(x_target1)
+set(gca,'FontSize',16,'xtick',[])
+ylabel('$\mathbf{z_1[n]}$', 'Interpreter', 'Latex','FontSize',22);
+subplot(3,1,2)
+plot(x_target2)
+set(gca,'FontSize',16,'xtick',[])
+ylabel('$\mathbf{z_2[n]}$', 'Interpreter', 'Latex','FontSize',22);
+subplot(3,1,3)
 plot(x_target) 
-set(gca,'FontSize',20)
-ylabel('$\mathbf{x_t[n]}$', 'Interpreter', 'Latex','FontSize',24);
-xlabel('\bf{n:te sampel}', 'Interpreter', 'Latex','FontSize',24);
-print('fig2','-dpdf');
+set(gca,'FontSize',16)
+ylabel('$\mathbf{x_t[n]}$', 'Interpreter', 'Latex','FontSize',22);
+xlabel('\bf{n:te sampel}', 'Interpreter', 'Latex','FontSize',22);
+%print('fig2','-dpdf');
 
 %fc i determined by looking at the
 %centrum of the heighest band of X
